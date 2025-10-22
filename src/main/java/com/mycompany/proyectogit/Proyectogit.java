30892db774185326d39cd8e142c97c59b379bff4
pmiranda1987 <percymiranda7@gmail.com>
2025-10-22 14:06:49 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/proyectogit/Proyectogit.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectogit;
+
+public class Proyectogit {
+
+    public static void main(String[] args) {
+       int a=20, b=25;
+       
+        System.out.println("la suma es: "+(a+b));
+    }
+}
